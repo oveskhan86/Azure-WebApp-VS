d891d1360c0babcb2376245b7d092ba665a05c79
Oves Khan <oveskhan86@gmail.com>
2025-09-28 20:42:17 -0600

    WIP: my important changes

--- obj/Debug/net8.0/azure-app-oves.AssemblyInfo.cs
+++ obj/Debug/net8.0/azure-app-oves.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azure-app-oves")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02384f29b915e3509573e36ac6eff765b633c7f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72d720ca56842a3bd1bdf54dd08175ea9dc52bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("azure-app-oves")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azure-app-oves")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
